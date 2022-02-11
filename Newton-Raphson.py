8ad1e96a2007002382cf334751b26124a24a2343
Finn Cartwright <finncartwright@8afbeed3.st-andrews.ac.uk>
2022-02-11 15:50:03 +0000

    Newton-Raphson.py

--- Newton-Raphson.py
+++ Newton-Raphson.py
@@ -1,4 +1,3 @@
-import numpy as np
 import sympy as sym
 
 #Define our functions of interest in form f(x,y) = 0
@@ -14,18 +13,18 @@ def newton_raphson(x0,y0,n,f,g):
         inverse_jacobian = jacobian ** -1
     except jacobian.det() == 0:
         raise TypeError('The matrix doesn\'t have an inverse')
+    #forms the matrices for the initial solution and functions
     function_matrix = sym.Matrix([[f],[g]])
     previous_solution = sym.Matrix([[x0],[y0]])
     print('Our 1st iterates are {}'.format(previous_solution))
+    #the actual method, it is very slow for sym functions but works very well for polynomials 
     for i in range(n):
         next_solution = previous_solution - (inverse_jacobian * function_matrix).subs({X:previous_solution[0],Y:previous_solution[1]})
         print('Our {}th iterates are {}'.format(i+2, next_solution.evalf()))
         previous_solution = next_solution
     
 
-
-
-newton_raphson(0.5,-0.5,5,F,G)
+newton_raphson(1,0.5,5,F,G)
 
 
 
